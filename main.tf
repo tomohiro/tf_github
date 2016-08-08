e4da1ebd50d671955171941143556e6fe7e23263
Tomohiro TAIRA <tomohiro.t@gmail.com>
2016-08-08 22:49:32 +0900

    Simplify take a value in map variable

--- main.tf
+++ main.tf
@@ -13,13 +13,13 @@ resource "github_membership" "membership_for_members" {
 resource "github_team" "secret_teams" {
   count       = "${length(var.secret_teams)}"
   name        = "${element(keys(var.secret_teams), count.index)}"
-  description = "${lookup(var.secret_teams, element(keys(var.secret_teams), count.index))}"
+  description = "${element(values(var.secret_teams), count.index)}"
   privacy     = "secret"
 }
 
 resource "github_team" "closed_teams" {
   count       = "${length(var.closed_teams)}"
   name        = "${element(keys(var.closed_teams), count.index)}"
-  description = "${lookup(var.closed_teams, element(keys(var.closed_teams), count.index))}"
+  description = "${element(values(var.closed_teams), count.index)}"
   privacy     = "closed"
 }
